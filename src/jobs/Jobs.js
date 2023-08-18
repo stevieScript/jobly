df453f28febf1e1986da9e8b8f12ce70ec4904ed
Stevie <steviescripts52@gmail.com>
2023-08-17 21:00:24 -0700

    job search form working

--- src/jobs/Jobs.js
+++ src/jobs/Jobs.js
@@ -1,12 +1,18 @@
 import {useState, useContext, useEffect} from 'react';
 import JobDetail from './JobDetail';
 import JoblyApi from '../api';
+import SearchForm from '../SearchForm';
 import UserContext from '../auth/UserContext';
 import {Navigate} from 'react-router-dom';
 function Jobs() {
 	const {currentUser} = useContext(UserContext);
 
 	const [jobs, setJobs] = useState([]);
+
+	const search = async (searchTerm) => {
+		const jobs = await JoblyApi.getJobs(searchTerm);
+		setJobs(jobs);
+	};
 
 	useEffect(() => {
 		const getJobs = async () => {
@@ -22,6 +28,8 @@ function Jobs() {
 
 	return (
 		<div className='Jobs'>
+			<SearchForm search={search} />
+			{jobs.length === 0 && <h3>No results found</h3>}
 			{jobs.map((job) => (
 				<JobDetail key={job.id} job={job} />
 			))}
